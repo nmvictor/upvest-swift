9cd6143b40a0d9dccdb150595803f3d77c88cd0d
Victor Moin <nmvictors@gmail.com>
2019-08-19 13:39:32 +0300

    Initial Commit

---
+++ Upvest/Upvest.h
@@ -0,0 +1,19 @@
+//
+//  Upvest.h
+//  Upvest
+//
+//  Created by Moin' Victor on 19/08/2019.
+//  Copyright © 2019 Upvest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Upvest.
+FOUNDATION_EXPORT double UpvestVersionNumber;
+
+//! Project version string for Upvest.
+FOUNDATION_EXPORT const unsigned char UpvestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Upvest/PublicHeader.h>
+
+
